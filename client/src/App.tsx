2e2456ddcb6c32f501330bd1fd9fbb25bd10cfda
Forrest Lin <forrest.lin.developer@gmail.com>
2023-03-28 23:53:36 +1100

    add backend host using render.com

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -58,15 +58,29 @@ function App() {
 
       // Save user to the database
       if (profileObj) {
-        const response = await fetch("http://localhost:8080/api/v1/users", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            name: profileObj.name,
-            email: profileObj.email,
-            avatar: profileObj.picture,
-          }),
-        });
+        const response = await fetch(
+          "https://realty-hive.onrender.com/api/v1/users",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              name: profileObj.name,
+              email: profileObj.email,
+              avatar: profileObj.picture,
+            }),
+          }
+        );
+        
+        // Local Server Test
+        // const response = await fetch("http://localhost:8080/api/v1/users", {
+        //   method: "POST",
+        //   headers: { "Content-Type": "application/json" },
+        //   body: JSON.stringify({
+        //     name: profileObj.name,
+        //     email: profileObj.email,
+        //     avatar: profileObj.picture,
+        //   }),
+        // });
 
         const data = await response.json();
         if (response.status === 200) {
@@ -127,7 +141,8 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://realty-hive.onrender.com/api/v1")}
+          // dataProvider={dataProvider("http://localhost:8080/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
